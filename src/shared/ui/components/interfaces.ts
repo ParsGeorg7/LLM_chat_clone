93957aac3b4bf3ce8e4e959e585409a399d03ca8
ParsGeorg7 <123315521+ParsGeorg7@users.noreply.github.com>
2024-12-16 23:37:34 +0300

    Update interfaces.ts

--- src/shared/ui/components/interfaces.ts
+++ src/shared/ui/components/interfaces.ts
@@ -3,6 +3,6 @@ import { UseControllerProps } from 'react-hook-form';
 
 export interface IProps extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>, UseControllerProps {
     name: string;
-    control: any;
+    control?: any;
     defaultValue?: string;
 }
